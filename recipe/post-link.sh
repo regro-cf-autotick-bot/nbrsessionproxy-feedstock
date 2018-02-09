3a4ea3dbc7fb16686951ccd683c3626a84683e1d
lsetiawan <landungs@uw.edu>
2018-02-09 12:21:31 -0800

    Update hooks

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,1 +1,3 @@
+"${PREFIX}/bin/jupyter-serverextension" enable nbrsessionproxy --py --sys-prefix
+"${PREFIX}/bin/jupyter-nbextension" install nbrsessionproxy --py --sys-prefix
 "${PREFIX}/bin/jupyter-nbextension" enable nbrsessionproxy --py --sys-prefix
--- recipe/pre-unlink.bat
+++ recipe/pre-unlink.bat
@@ -1,3 +1,5 @@
 @echo off
 
+"%PREFIX%\Scripts\jupyter-serverextension.exe" disable nbrsessionproxy --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" uninstall nbrsessionproxy --py --sys-prefix && if errorlevel 1 exit 1
 "%PREFIX%\Scripts\jupyter-nbextension.exe" disable nbrsessionproxy --py --sys-prefix && if errorlevel 1 exit 1
